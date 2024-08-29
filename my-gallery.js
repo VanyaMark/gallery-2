3d58030d8d4ad8bb5eb13ce2bf391b554cc86d8c
VanyaMark <markova.vanya@gmail.com>
2024-08-29 09:11:12 +0200

    first commit

---
+++ my-gallery.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    effect: 'flip',
+
+  });
+
+  const { styler, spring, listen, pointer, value } = window.popmotion;
+
+  const ball = document.querySelector('.brand');
+  const divStyler = styler(ball);
+  const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+  
+  listen(ball, 'mousedown touchstart')
+    .start((e) => {
+      e.preventDefault();
+      pointer(ballXY.get()).start(ballXY);
+    });
+  
+  listen(document, 'mouseup touchend')
+    .start(() => {
+      spring({
+        from: ballXY.get(),
+        velocity: ballXY.getVelocity(),
+        to: { x: 0, y: 0 },
+        stiffness: 200,
+        // mass: 1,
+        // damping: 10
+      }).start(ballXY);
+    });
